d080b901664ab8c97f107cd228fe35c7788bd469
zerrickstaples <zerrick.staples@gmail.com>
2016-10-28 08:40:26 -0400

    added files via upload

---
+++ WeatherApp/Models/Weather.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Script.Serialization;
+
+namespace WeatherApp.Models
+{
+    public class Weather
+    {
+        public Object getWeatherForcast()
+        {
+            string url = "http://api.openweathermap.org/data/2.5/weather?q=Cleveland&APPID=cbbcd2aff061521e97792f80e13c0d5f&units=imperial";
+            //synchronus client
+            var client = new System.Net.WebClient();
+            var content = client.DownloadString(url);
+            var serializer = new JavaScriptSerializer();
+            var jsonContent = serializer.Deserialize<object>(content);
+            return jsonContent;
+        }
+    }
+}
